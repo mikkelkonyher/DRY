f4ea3ab87c3478a3edc32285fbb683a08e858ad7
Mikkel konyher <146730338+mikkelkonyher@users.noreply.github.com>
2024-11-24 10:55:16 +0100

    s

--- DRYV1/DRYV1/obj/Debug/net8.0/DRYV1.AssemblyInfo.cs
+++ DRYV1/DRYV1/obj/Debug/net8.0/DRYV1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DRYV1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a318c317a153ffc5e080de9b8b5b2d5089557e0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39d5f6b3c13f43195245909bee4f8863f2cd3284")]
 [assembly: System.Reflection.AssemblyProductAttribute("DRYV1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DRYV1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
