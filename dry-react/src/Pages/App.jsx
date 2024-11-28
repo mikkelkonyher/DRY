2170b87eafde0bf949bacc59cea73321057e5b46
Mikkel konyher <146730338+mikkelkonyher@users.noreply.github.com>
2024-11-28 09:57:34 +0100

    s

---
+++ dry-react/src/Pages/App.jsx
@@ -0,0 +1,56 @@
+import { Route, Routes } from 'react-router-dom';
+import CreateGuitBassGear from "./AllMusicGear/GuitBassGear/CreateGuitBassGear.jsx";
+import GetGuitBassGear from "./AllMusicGear/GuitBassGear/GetGuitBassGear.jsx";
+import CreateDrumsGear from "./AllMusicGear/DrumsGear/CreateDrumsGear.jsx";
+import GetDrumsGear from "./AllMusicGear/DrumsGear/GetDrumsGear.jsx";
+import CreateStudioGear from "./AllMusicGear/StudioGear/CreateStudioGear.jsx";
+import GetStudioGear from "./AllMusicGear/StudioGear/GetStudioGear.jsx";
+import CreateKeysGear from ".//AllMusicGear/KeysGear/CreateKeysGear.jsx";
+import GetKeysGear from "./AllMusicGear/KeysGear/GetKeysGear.jsx";
+import CreateStringsGear from "./AllMusicGear/StringsGear/CreateStringsGear.jsx";
+import GetStringsGear from "./AllMusicGear/StringsGear/GetStringsGear.jsx";
+import CreateHornsGear from "./AllMusicGear/HornsGear/CreateHornsGear.jsx";
+import GetHornsGear from "./AllMusicGear/HornsGear/GetHornsGear.jsx";
+import CardDetails from "./Forms/CardDetails.jsx";
+import ResponsiveAppBar from './FooterHeader/Navbar';
+import Box from '@mui/material/Box';
+import Home from "./Home.jsx";
+import Login from './LoginSignup/Login.jsx';
+import Signup from "./LoginSignup/Signup.jsx";
+import ForgotPassword from "./LoginSignup/ForgotPassword.jsx";
+import ResetPassword from "./LoginSignup/ResetPassword.jsx";
+import MyProfile from "./User/MyProfile.jsx";
+
+
+function App() {
+    return (
+        <>
+            <ResponsiveAppBar />
+            <Box sx={{ marginTop: '84px', padding: '20px' }}>
+                <Routes>
+                    <Route path="/SellGuiBassGear" element={<CreateGuitBassGear />} />
+                    <Route path="/SellDrumsGear" element={<CreateDrumsGear />} />
+                    <Route path="/SellStudioGear" element={<CreateStudioGear />} />
+                    <Route path="/SellKeysGear" element={<CreateKeysGear />} />
+                    <Route path="/SellStringsGear" element={<CreateStringsGear />} />
+                    <Route path="/SellHornsGear" element={<CreateHornsGear />} />
+                    <Route path="/GuitBass" element={<GetGuitBassGear />} />
+                    <Route path="/Trommer" element={<GetDrumsGear />} />
+                    <Route path="/Studiogear" element={<GetStudioGear />} />
+                    <Route path="/Strygere" element={<GetStringsGear />} />
+                    <Route path="/Keys" element={<GetKeysGear />} />
+                    <Route path="/Blæsere" element={<GetHornsGear />} />
+                    <Route path="/gear/:id" element={<CardDetails />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/" element={<Home />} />
+                    <Route path="/forgot-password" element={<ForgotPassword />} />
+                    <Route path="/reset-password/:token" element={<ResetPassword />}/>
+                    <Route path="/myprofile" element={<MyProfile />} />
+                </Routes>
+            </Box>
+        </>
+    );
+}
+
+export default App;
--- dry-react/src/main.jsx
+++ dry-react/src/main.jsx
@@ -1,67 +1,22 @@
 import { StrictMode, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import CreateGuitBassGear from './Pages/AllMusicGear/GuitBassGear/CreateGuitBassGear.jsx';
-import GetGuitBassGear from './Pages/AllMusicGear/GuitBassGear/GetGuitBassGear.jsx';
-import CreateDrumsGear from "./Pages/AllMusicGear/DrumsGear/CreateDrumsGear.jsx";
-import GetDrumsGear from "./Pages/AllMusicGear/DrumsGear/GetDrumsGear.jsx";
-import CreateStudioGear from "./Pages/AllMusicGear/StudioGear/CreateStudioGear.jsx";
-import GetStudioGear from "./Pages/AllMusicGear/StudioGear/GetStudioGear.jsx";
-import CreateKeysGear from "./Pages/AllMusicGear/KeysGear/CreateKeysGear.jsx";
-import GetKeysGear from "./Pages/AllMusicGear/KeysGear/GetKeysGear.jsx";
-import CreateStringsGear from "./Pages/AllMusicGear/StringsGear/CreateStringsGear.jsx";
-import GetStringsGear from "./Pages/AllMusicGear/StringsGear/GetStringsGear.jsx";
-import CreateHornsGear from "./Pages/AllMusicGear/HornsGear/CreateHornsGear.jsx";
-import GetHornsGear from "./Pages/AllMusicGear/HornsGear/GetHornsGear.jsx";
-import CardDetails from "./Pages/Forms/CardDetails.jsx";
-import ResponsiveAppBar from './Pages/FooterHeader/Navbar';
+import { BrowserRouter } from 'react-router-dom';
+import App from './Pages/App';
 import './index.css';
-import Box from '@mui/material/Box';
-import Home from './Pages/Home/';
-import Login from './Pages/LoginSignup/Login.jsx';
-import Signup from "./Pages/LoginSignup/Signup.jsx";
 import { checkToken } from './authUtils';
-import ForgotPassword from "./Pages/LoginSignup/ForgotPassword.jsx";
-import ResetPassword from "./Pages/LoginSignup/ResetPassword.jsx";
-import MyProfile from "./Pages/User/MyProfile.jsx";
 
 function Main() {
     useEffect(() => {
         checkToken();
     }, []);
 
-    return (
-        <Router>
-            <ResponsiveAppBar />
-            <Box sx={{ marginTop: '84px', padding: '20px' }}>
-                <Routes>
-                    <Route path="/SellGuiBassGear" element={<CreateGuitBassGear />} />
-                    <Route path="/SellDrumsGear" element={<CreateDrumsGear />} />
-                    <Route path="/SellStudioGear" element={<CreateStudioGear />} />
-                    <Route path="/SellKeysGear" element={<CreateKeysGear />} />
-                    <Route path="/SellStringsGear" element={<CreateStringsGear />} />
-                    <Route path="/SellHornsGear" element={<CreateHornsGear />} />
-                    <Route path="/GuitBass" element={<GetGuitBassGear />} />
-                    <Route path="/Trommer" element={<GetDrumsGear />} />
-                    <Route path="/Studiogear" element={<GetStudioGear />} />
-                    <Route path="/Strygere" element={<GetStringsGear />} />
-                    <Route path="/Keys" element={<GetKeysGear />} />
-                    <Route path="/Blæsere" element={<GetHornsGear />} />
-                    <Route path="/gear/:id" element={<CardDetails />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/signup" element={<Signup />} />
-                    <Route path="/" element={<Home />} />
-                    <Route path="/forgot-password" element={<ForgotPassword />} />
-                    <Route path="/reset-password/:token" element={<ResetPassword />}/>
-                    <Route path="/myprofile" element={<MyProfile />} />
-                </Routes>
-            </Box>
-        </Router>
-    );
+    return <App />;
 }
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <Main />
+        <BrowserRouter>
+            <Main />
+        </BrowserRouter>
     </StrictMode>,
 );
