4f36712055d7f41d6769a2b61d727a00c7179a38
Mikkel konyher <146730338+mikkelkonyher@users.noreply.github.com>
2024-12-10 09:23:49 +0100

    migrationPgAdmin

---
+++ DRYV1/DRYV1/Migrations/20241210081158_Initial.cs
@@ -0,0 +1,413 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DRYV1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Forums",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Subject = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Body = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    LikeCount = table.Column<int>(type: "integer", nullable: false),
+                    ImagePaths = table.Column<List<string>>(type: "text[]", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Forums", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    IsValidated = table.Column<bool>(type: "boolean", nullable: false),
+                    ProfileImageUrl = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ForumLikes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    ForumId = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ForumLikes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ForumLikes_Forums_ForumId",
+                        column: x => x.ForumId,
+                        principalTable: "Forums",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MusicGear",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ListingDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Brand = table.Column<string>(type: "text", nullable: false),
+                    Model = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false),
+                    Location = table.Column<string>(type: "text", nullable: false),
+                    Condition = table.Column<string>(type: "text", nullable: false),
+                    Year = table.Column<int>(type: "integer", nullable: false),
+                    ImagePaths = table.Column<List<string>>(type: "text[]", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    FavoriteCount = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MusicGear", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MusicGear_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RehearsalRooms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Address = table.Column<string>(type: "text", nullable: false),
+                    Location = table.Column<string>(type: "text", nullable: false),
+                    ListingDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    PaymentType = table.Column<string>(type: "text", nullable: false),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false),
+                    RoomSize = table.Column<int>(type: "integer", nullable: false),
+                    Type = table.Column<string>(type: "text", nullable: false),
+                    FavoriteCount = table.Column<int>(type: "integer", nullable: false),
+                    ImagePaths = table.Column<List<string>>(type: "text[]", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RehearsalRooms", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RehearsalRooms_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DrumsGear",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false),
+                    DrumsGearType = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DrumsGear", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DrumsGear_MusicGear_Id",
+                        column: x => x.Id,
+                        principalTable: "MusicGear",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Favorites",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    MusicGearId = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Favorites", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Favorites_MusicGear_MusicGearId",
+                        column: x => x.MusicGearId,
+                        principalTable: "MusicGear",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GuitBassGear",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false),
+                    GuitBassType = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GuitBassGear", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GuitBassGear_MusicGear_Id",
+                        column: x => x.Id,
+                        principalTable: "MusicGear",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HornsGear",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false),
+                    HornsGearType = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HornsGear", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_HornsGear_MusicGear_Id",
+                        column: x => x.Id,
+                        principalTable: "MusicGear",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "KeysGear",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false),
+                    KeysGearType = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KeysGear", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_KeysGear_MusicGear_Id",
+                        column: x => x.Id,
+                        principalTable: "MusicGear",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StringsGear",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false),
+                    StringsGearType = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StringsGear", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StringsGear_MusicGear_Id",
+                        column: x => x.Id,
+                        principalTable: "MusicGear",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StudioGear",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false),
+                    StudioGearType = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudioGear", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StudioGear_MusicGear_Id",
+                        column: x => x.Id,
+                        principalTable: "MusicGear",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    MusicGearId = table.Column<int>(type: "integer", nullable: true),
+                    RehearsalRoomId = table.Column<int>(type: "integer", nullable: true),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    Text = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ForumId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comments_Forums_ForumId",
+                        column: x => x.ForumId,
+                        principalTable: "Forums",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Comments_MusicGear_MusicGearId",
+                        column: x => x.MusicGearId,
+                        principalTable: "MusicGear",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Comments_RehearsalRooms_RehearsalRoomId",
+                        column: x => x.RehearsalRoomId,
+                        principalTable: "RehearsalRooms",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Comments_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RehearsalRoomFavorites",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    RehearsalRoomid = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RehearsalRoomFavorites", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RehearsalRoomFavorites_RehearsalRooms_RehearsalRoomid",
+                        column: x => x.RehearsalRoomid,
+                        principalTable: "RehearsalRooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_ForumId",
+                table: "Comments",
+                column: "ForumId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_MusicGearId",
+                table: "Comments",
+                column: "MusicGearId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_RehearsalRoomId",
+                table: "Comments",
+                column: "RehearsalRoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_UserId",
+                table: "Comments",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Favorites_MusicGearId",
+                table: "Favorites",
+                column: "MusicGearId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ForumLikes_ForumId",
+                table: "ForumLikes",
+                column: "ForumId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MusicGear_UserId",
+                table: "MusicGear",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RehearsalRoomFavorites_RehearsalRoomid",
+                table: "RehearsalRoomFavorites",
+                column: "RehearsalRoomid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RehearsalRooms_UserId",
+                table: "RehearsalRooms",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "DrumsGear");
+
+            migrationBuilder.DropTable(
+                name: "Favorites");
+
+            migrationBuilder.DropTable(
+                name: "ForumLikes");
+
+            migrationBuilder.DropTable(
+                name: "GuitBassGear");
+
+            migrationBuilder.DropTable(
+                name: "HornsGear");
+
+            migrationBuilder.DropTable(
+                name: "KeysGear");
+
+            migrationBuilder.DropTable(
+                name: "RehearsalRoomFavorites");
+
+            migrationBuilder.DropTable(
+                name: "StringsGear");
+
+            migrationBuilder.DropTable(
+                name: "StudioGear");
+
+            migrationBuilder.DropTable(
+                name: "Forums");
+
+            migrationBuilder.DropTable(
+                name: "RehearsalRooms");
+
+            migrationBuilder.DropTable(
+                name: "MusicGear");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- DRYV1/DRYV1/obj/Debug/net8.0/DRYV1.AssemblyInfo.cs
+++ DRYV1/DRYV1/obj/Debug/net8.0/DRYV1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DRYV1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b44690ba8c0bb3c17c06c7c5cd2cfee1334c9034")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0f412864c7cfd1eeb2af71fe5006639e259709b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DRYV1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DRYV1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
