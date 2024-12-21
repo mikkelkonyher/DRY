cd7f9cfc3b867c8f99617b110531d7dc2fad7d61
Mikkel konyher <146730338+mikkelkonyher@users.noreply.github.com>
2024-12-21 10:27:44 +0100

    redirects

--- DRYV1/DRYV1/obj/Debug/net8.0/DRYV1.AssemblyInfo.cs
+++ DRYV1/DRYV1/obj/Debug/net8.0/DRYV1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DRYV1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceb84c3f016b3eb2ca20cfc1d152c01d07714526")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+457655ba01e4ab6c60078f4761476f695678917e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DRYV1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DRYV1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
