397b82953784688c9be3710b8696a4ad0bf1bd92
Mikkel konyher <146730338+mikkelkonyher@users.noreply.github.com>
2025-01-28 13:42:26 +0100

    removeGearCardCss

--- DRYV1/DRYV1/obj/Debug/net8.0/DRYV1.AssemblyInfo.cs
+++ DRYV1/DRYV1/obj/Debug/net8.0/DRYV1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DRYV1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f403365da17a68dc28ff290d2a78ee01dfac3ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b741d1807795247a3e3ff5a2f92991015ca822e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("DRYV1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DRYV1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- dry-react/src/Pages/Forms/Gear/GearCard.jsx
+++ dry-react/src/Pages/Forms/Gear/GearCard.jsx
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart as solidHeart } from '@fortawesome/free-solid-svg-icons';
 import { faHeart as regularHeart } from '@fortawesome/free-regular-svg-icons';
 import config from "../../../../config.jsx";
-import './GearCard.css';
+
 
 function GearCard({ item, handleImageClick, userId }) {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
