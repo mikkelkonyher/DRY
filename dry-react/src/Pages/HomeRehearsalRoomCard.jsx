23e28b975a9032c587366272595885bcfeee74b9
Mikkel konyher <146730338+mikkelkonyher@users.noreply.github.com>
2025-01-29 11:13:51 +0100

    HomeCards

--- dry-react/src/Pages/HomeRehearsalRoomCard.jsx
+++ dry-react/src/Pages/HomeRehearsalRoomCard.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
 import './HomeRehearsalRoomCard.css';
 
 function HomeRehearsalRoomCard({ item }) {
+    const navigate = useNavigate();
+
+    const handleCardClick = () => {
+        navigate(`/RehearsalRoomDetails/${item.id}`);
+    };
+
     return (
-        <div className="Homerehearsal-room-card">
+        <div className="Homerehearsal-room-card" onClick={handleCardClick}>
             {item.imagePaths && item.imagePaths.length > 0 ? (
                 <img className="Homerehearsal-room-image" src={item.imagePaths[0]} alt={item.name} />
             ) : (
@@ -14,7 +21,7 @@ function HomeRehearsalRoomCard({ item }) {
                 <h3>{item.name}</h3>
             </div>
             <div className="Homerehearsal-room-price">
-                <h4>{item.price}</h4>
+                <h4>{item.price} DKK {item.paymentType}</h4>
             </div>
         </div>
     );
