cd17f5549c4985d2cad603d56679556033b19443
Mikkel konyher <146730338+mikkelkonyher@users.noreply.github.com>
2025-02-04 12:15:40 +0100

    Update GetForumForm.jsx

--- dry-react/src/Pages/Forms/Forum/GetForumForm.jsx
+++ dry-react/src/Pages/Forms/Forum/GetForumForm.jsx
@@ -109,16 +109,8 @@ function GetForumForm() {
 
     const totalPages = Math.ceil(totalItems / itemsPerPage);
 
-    const handlePageChange = (direction) => {
-        setCurrentPage((prevPage) => {
-            let newPage = prevPage;
-            if (direction === 'prev' && prevPage > 1) {
-                newPage = prevPage - 1;
-            } else if (direction === 'next' && prevPage < totalPages) {
-                newPage = prevPage + 1;
-            }
-            return newPage;
-        });
+    const handlePageChange = (pageNumber) => {
+        setCurrentPage(pageNumber);
     };
 
     return (
