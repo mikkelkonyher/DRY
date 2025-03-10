94979cd7c67aac7723068ec2b7138f6ad5d61a6f
Mikkel konyher <146730338+mikkelkonyher@users.noreply.github.com>
2025-03-10 10:29:11 +0100

    config&programcs

--- dry-react/config.jsx
+++ dry-react/config.jsx
@@ -1,5 +1,9 @@
+//'https://localhost:7064'
+
+//'https://gearninja-api-g8a6c8brfkcyc5bk.northeurope-01.azurewebsites.net'
+
 const config = {
-    apiBaseUrl: 'https://gearninja-api-g8a6c8brfkcyc5bk.northeurope-01.azurewebsites.net'
+    apiBaseUrl: 'https://localhost:7064'
 };
 
 export default config;
