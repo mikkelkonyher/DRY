2231ecd08ca82d72e86c101774f95bb5933039c5
Mikkel konyher <146730338+mikkelkonyher@users.noreply.github.com>
2025-06-13 10:42:36 +0200

    forumLoadingButton

--- DRYV1/DRYV1/obj/Debug/net8.0/DRYV1.AssemblyInfo.cs
+++ DRYV1/DRYV1/obj/Debug/net8.0/DRYV1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DRYV1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fcfbf1cb125d545dad4fe3f18344a280de0db34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27d3a3a7e032374fdd0b29a6216f309ca5926998")]
 [assembly: System.Reflection.AssemblyProductAttribute("DRYV1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DRYV1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
