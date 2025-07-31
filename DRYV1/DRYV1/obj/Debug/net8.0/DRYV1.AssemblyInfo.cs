3c81bee4d44f0f5e5b1562d7a6a7e8a5b48ee687
Mikkel konyher <146730338+mikkelkonyher@users.noreply.github.com>
2025-07-31 09:32:03 +0200

    updateSixlaborsImageSharp

--- DRYV1/DRYV1/obj/Debug/net8.0/DRYV1.AssemblyInfo.cs
+++ DRYV1/DRYV1/obj/Debug/net8.0/DRYV1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DRYV1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f2dc4492e5eae59a82862bf5cb09f237aed545c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b84b01c0f051b3c68235535b73c6921f13a55ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("DRYV1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DRYV1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
